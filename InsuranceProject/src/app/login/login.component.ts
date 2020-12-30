af4488b5f14791404be79d0dbb430a3f473a4105
Kittitorn Rakpanyakaeo <kittitornrak@mathregit.group>
2020-12-30 15:43:22 +0700

    fixed login popup

--- InsuranceProject/src/app/login/login.component.ts
+++ InsuranceProject/src/app/login/login.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { PageControllerService } from '../page-controller.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private pageService: PageControllerService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  closeClick() {
+    this.pageService.isLoginPage = false;
   }
-
 }
