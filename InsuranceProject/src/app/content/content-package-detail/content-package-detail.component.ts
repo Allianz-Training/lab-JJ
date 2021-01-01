2f301c38bb59e7597983e562406af1a89d51789b
Kittitorn Rakpanyakaeo <kittitornrak@mathregit.group>
2021-01-01 17:49:21 +0700

    Pass Package value by Object

--- InsuranceProject/src/app/content/content-package-detail/content-package-detail.component.ts
+++ InsuranceProject/src/app/content/content-package-detail/content-package-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Package } from 'src/app/package';
 
 @Component({
   selector: 'app-content-package-detail',
@@ -6,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-package-detail.component.css'],
 })
 export class ContentPackageDetailComponent implements OnInit {
+  package: Package;
   id: number;
-  constructor() {}
+  constructor() {
+    if (this.package == null) {
+      this.package = new Package(
+        99,
+        'Unknown Package',
+        ['a', 'b', 'c'],
+        '120',
+        1000
+      );
+    }
+  }
 
   ngOnInit(): void {}
 }
