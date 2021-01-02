6759379528620ac93f4979682f0a34c729c6f080
ksillapapibul <ksillapapibul@gmail.com>
2021-01-02 20:14:54 +0700

    edit spec

--- InsuranceProject/src/app/content/content-payment/content-payment.component.spec.ts
+++ InsuranceProject/src/app/content/content-payment/content-payment.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PageControllerService } from 'src/app/page-controller.service';
 
 import { ContentPaymentComponent } from './content-payment.component';
 
@@ -8,7 +11,9 @@ describe('ContentPaymentComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ContentPaymentComponent ]
+      imports:[RouterTestingModule],
+      declarations: [ ContentPaymentComponent ],
+      providers:[PageControllerService,{provide: ActivatedRoute, useValue: ({id: '1'})}]
     })
     .compileComponents();
   });
